ff4bf3b49d4140d556a96c76a765082318a68fc0
mosahel01 <mos.sahil01@gmail.com>
2026-06-15 21:36:54 +0530

    add: directory symlinking

--- install.sh
+++ install.sh
@@ -1,32 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
+
 DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 
-echo "Installing Dotfiles.."
+echo "Installing Dotfiles..."
 
-rm -rf "$HOME/.config/ghostty"
+# ghostty
 mkdir -p "$HOME/.config/ghostty"
 ln -sfn "$DOTFILES/config.ghostty" "$HOME/.config/ghostty/config"
 
-echo "ghostty installed"
+# zsh
+ln -sfn "$DOTFILES/zsh" "$HOME/.config/zsh"
 
-rm -rf "$HOME/.config/zsh"
-rm -rf "$HOME/.zshrc"
+# neovim
+ln -sfn "$DOTFILES/nvim" "$HOME/.config/nvim"
 
-for file in "$DOTFILES"/zsh/*; do
-	ln -sfn "$file" "$HOME/.config/zsh/$(basename "$file")"
-done
-
-echo "zsh installed"
-
-
-rm -rf "$HOME/.config/nvim"
-mkdir -p "$HOME/.config/nvim"
-for file in "$DOTFILES"/nvim/*; do
-	ln -sfn "$file" "$HOME/.config/nvim/$(basename "$file")"
-done
-
-echo "nvim installed"
-
-
-
+echo "Done."
